1ee87ab7f925c798e29a178f16c10ec1f3e43239
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2022-11-15 19:58:49 +0100

    Add project files.

---
+++ Semestr1_Lekcja8/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semestr1_Lekcja8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //program który tłumaczy cyfry na dni tygodnia słownie
+            // 1 - poniedziałek
+            // 2 - wtorek
+            // ...  7 - niedziela
+
+            Console.WriteLine("Podaj numer tygodnia (1-7):");
+            int dzien = int.Parse(Console.ReadLine());
+
+            if (dzien == 1)
+            {
+                Console.WriteLine("poniedziałek");
+            }
+            else if(dzien == 2)
+            {
+                Console.WriteLine("Wtorek");
+            }
+            else if (dzien == 3)
+            {
+                Console.WriteLine("Środa");
+            }
+            else if (dzien == 4)
+            {
+                Console.WriteLine("Czwartek");
+            }
+            else if (dzien == 5)
+            {
+                Console.WriteLine("Piątek");
+            }
+            else if (dzien == 6)
+            {
+                Console.WriteLine("Sobota");
+            }
+            else if (dzien == 7)
+            {
+                Console.WriteLine("Niedziela");
+            }
+            else
+            {
+                Console.WriteLine("Niepoprawna liczba");
+            }
+
+            //INSTRUKCJA SWITCH 
+
+            switch (dzien)
+            {
+                case 1:
+                    Console.WriteLine("Poniedziałek");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Wtorek");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Środa");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Czwartek");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Piątek");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Sobota");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Niedziela");
+                    break;
+
+                default:
+                    Console.WriteLine("Niepoprawna liczba");
+                    break;   
+            }
+
+            Console.WriteLine("Podaj ocenę w systemie amerykańskim");
+            //A - 5,  B - 4, C - 3, D - 2, F - 1
+            
+            char ocena = Char.Parse(Console.ReadLine());
+
+            switch (ocena)
+            {
+                case 'A':
+                    Console.WriteLine("5 - bardzo dobry");
+                    break;
+
+                case 'B':
+                    Console.WriteLine("4 - dobry");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("3 - dostateczny");
+                    break;
+
+                case 'D':
+                    Console.WriteLine("2 - dopuszczający");
+                    break;
+
+                case 'F':
+                    Console.WriteLine("1 - niedostateczny");
+                    break;
+
+                default :
+                    Console.WriteLine("Podana wartość jest nieporawidłowa");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ćwiczenie1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Ćwiczenie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj Łowco, na jakiego potwora polujesz?");
+
+            string potwor = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"{potwor} mówisz... Niech pomyślę...");
+            Thread.Sleep(3000);
+            Console.Clear();
+            // Gryf =>  gryf       GRYF =>   gryf     grYF  =>  gryf
+            switch (potwor.ToLower())
+            {
+                case "gryf":
+                    Console.WriteLine("Gryf z wyglądu jest połączeniem lwa (ogon i");
+                    Console.WriteLine("sierść) oraz orła(ostry, zakrzywiony dziób, pazury i ogromne skrzydła)");
+                    Console.WriteLine("HP: 1000");
+                    Console.WriteLine("Szybkość: 80");
+                    break;
+
+                case "bies":
+                    Console.WriteLine("W oczach przeciętnego mieszkańca Królestw Północy");
+                    Console.WriteLine("bies jest ucieleśnieniem wszystkiego co złe i diabelskie,"); 
+                    Console.WriteLine("co możnazresztą zauważyć w wielu znanych porzekadłach.");
+                    Console.WriteLine("HP: 5000");
+                    Console.WriteLine("Szybkość: 90");
+                    break;
+
+                default:
+                    Console.WriteLine("Takiego potwora to ja nie znam");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
